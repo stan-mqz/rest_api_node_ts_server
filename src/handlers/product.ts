869841a31b10d08306c9118e8a71c6160ae30ae5
Stan <moscarstanley37@gmail.com>
2025-01-17 20:36:21 -0600

    feat: add express-validator for product creation validation

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,8 +1,26 @@
 import { Request, Response } from "express"
+import { check, validationResult } from "express-validator"
 import Product from "../models/Product.model"
 
 export const createProduct = async (req : Request , res : Response) => {
     
+
+    //Validacion
+    await check('name').notEmpty().withMessage('El nombre del producto no debe estar vacio')
+    .run(req)
+    await check('price')
+        .isNumeric().withMessage('Valor no valido')
+        .notEmpty().withMessage('El precio del producto no debe estar vacio')
+        .custom(value => value > 0).withMessage('El precio no puede ser negativo')
+        .run(req)
+        
+
+    let errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})    
+    }
+
     const product = await Product.create(req.body)
     res.json({data: product})
 }
