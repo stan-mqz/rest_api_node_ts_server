65244f285963873aa81c3b7ab4952f90143c28a4
Stan <moscarstanley37@gmail.com>
2025-01-27 15:14:05 -0600

    feat: create validation test to avoid accepting empty requests

--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -2,6 +2,14 @@ import request from 'supertest'
 import server from '../../server'
 
 describe('POST / Product', () => {
+
+    test('It should display validation errors', async () => {
+        const response = await request(server).post('/products').send({})
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(4)
+    })
+
    test('It should create a new product', async () => {
     const response = await request(server).post('/products').send({
         name: 'Product 1 - Testing',
@@ -9,6 +17,8 @@ describe('POST / Product', () => {
     })
 
     expect(response.status).toBe(201)
+    expect(response.status).not.toBe(404)
     expect(response.body).toHaveProperty('data')
+    expect(response.body).not.toHaveProperty('errors')
    })
 })
