4e28aff903ca2c4463599f6f5c06e7464dba1523
Stan <moscarstanley37@gmail.com>
2025-01-27 15:47:26 -0600

    test: update product API tests; rename routes and add validation for product retrieval

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -2,10 +2,10 @@ import request from "supertest";
 import server from "../server";
 
 
-describe('GET /products', () => {
+describe('GET /api', () => {
     test('It should send back a json response', async () => {
 
-        const res = await request(server).get('/products')
+        const res = await request(server).get('/api')
         
         expect(res.status).toBe(200)
         expect(res.headers['content-type']).toMatch(/json/)
--- src/handlers/__tests__/product.test.ts
+++ src/handlers/__tests__/product.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest'
 import server from '../../server'
 
-describe('POST / Product', () => {
+describe('POST / products', () => {
 
     test('It should display validation errors', async () => {
         const response = await request(server).post('/products').send({})
@@ -41,4 +41,23 @@ describe('POST / Product', () => {
     expect(response.body).toHaveProperty('data')
     expect(response.body).not.toHaveProperty('errors')
    })
+})
+
+describe('GET /products/id', () => {
+
+    test('It should check if /products url exists', async () => {
+        const response = await request(server).get('/products')
+        expect(response.status).not.toBe(404)
+    })
+
+    test('It should return a JSON with products', async () => {
+        const response = await request(server).get('/products')
+        expect(response.status).toBe(200)
+        expect(response.headers['content-type']).toMatch(/json/)
+        expect(response.body).toHaveProperty('data')
+        expect(response.body.data).toHaveLength(1)
+        expect(response.body).not.toHaveProperty('errors')
+        expect(response.status).not.toBe(404)
+
+    })
 })
